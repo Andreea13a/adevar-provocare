f4323e37753b08e402e7952bbf957067e076c7ff
Andreea13a <donosandreea5@gmail.com>
2025-04-15 12:26:22 +0300

    v gvhj

--- autentificare/autentificare.js
+++ autentificare/autentificare.js
@@ -11,6 +11,10 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     ) {
       document.getElementById("loginResult").textContent = "Autentificare reușită!";
       document.getElementById("loginResult").style.color = "black";
+      // Redirecționare către pagina principală
+      setTimeout(() => {
+        window.location.href = "../continut/text.html"; // Calea corectă către fișier
+      }, 1000);
     } else {
       document.getElementById("loginResult").textContent = "Email sau parolă incorectă.";
       document.getElementById("loginResult").style.color = "red";
---
+++ navbar/navbar.js
@@ -0,0 +1,5 @@
+// Logout button functionality
+document.querySelector(".logout-button").addEventListener("click", function () {
+    alert("Te-ai delogat cu succes!");
+    window.location.href = "../autentificare/index.html"; // Redirecționează către pagina de autentificare
+});
